5cbcb0f24268ee1c336f109e874f11d99ecf25b2
Sudo-Self <sudo-self@proton.me>
2025-05-19 02:05:09 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,9 +55,10 @@ export default function Home() {
         Create SEO metadata
       </DialogDescription>
     </DialogHeader>
-    <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <MetaTagMaker />
-    </div>
+    <div className="p-4 space-y-6 bg-background text-foreground">
+  <MetaTagMaker />
+</div>
+
   </DialogContent>
 </Dialog>
         </div>
